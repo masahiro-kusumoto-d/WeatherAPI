874cdff3733776e5192be1e1646c6375e3b94a81
masahiro_kusumoto <masahiro_kusumoto@dwango.co.jp>
2018-07-23 15:54:40 +0900

    a

--- test/test.js
+++ test/test.js
@@ -1,9 +1,7 @@
-var router = require("../routes/")
-var assert = require("assert")
+var assert = require("assert");
 
 describe('100% Success', function(){
   it('1+1=2', function(){
     assert.equal(1 + 1, 2);
-  })
-})
-
+  });
+});
