7671eaf7f508eb95a18b03a1382e2baeeba27749
masahiro_kusumoto <masahiro_kusumoto@dwango.co.jp>
2018-07-23 10:05:57 +0900

    Add greeter.ts

---
+++ greeter.ts
@@ -0,0 +1,22 @@
+
+enum Direction {
+  Up = "up",
+  Down = "down",
+}
+const num: Direction = Direction.Up
+console.log(num)
+
+
+interface B {
+  name: string
+}
+class MyClass {
+  name = ''
+}
+
+let myClass = new MyClass
+console.log(myClass)
+
+let b = <B> {}
+b.name = ''
+console.log(b)
